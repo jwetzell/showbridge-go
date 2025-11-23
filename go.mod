532a37816f746a76728736d7976fb82d3a188f3f
Joel Wetzell <git@jwetzell.com>
2025-11-22 18:07:15 -0600

    add basic mqtt client

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/jwetzell/showbridge-go
 go 1.25.1
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.1
 	github.com/jwetzell/osc-go v0.0.0-20251114203632-24077a77d6c7
 	github.com/urfave/cli/v3 v3.6.0
+)
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 )
---
+++ mqtt-client.go
@@ -0,0 +1,108 @@
+package showbridge
+
+import (
+	"fmt"
+	"log/slog"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type MQTTClient struct {
+	config   ModuleConfig
+	router   *Router
+	Broker   string
+	ClientID string
+	Topic    string
+	client   mqtt.Client
+}
+
+func init() {
+	RegisterModule(ModuleRegistration{
+		Type: "net.mqtt.client",
+		New: func(config ModuleConfig) (Module, error) {
+			params := config.Params
+			broker, ok := params["broker"]
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client requires a broker parameter")
+			}
+
+			brokerString, ok := broker.(string)
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client host must be string")
+			}
+
+			topic, ok := params["topic"]
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client requires a topic parameter")
+			}
+
+			topicString, ok := topic.(string)
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client host must be string")
+			}
+
+			clientId, ok := params["clientId"]
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client requires a clientId parameter")
+			}
+
+			clientIdString, ok := clientId.(string)
+
+			if !ok {
+				return nil, fmt.Errorf("net.mqtt.client host must be string")
+			}
+
+			return &MQTTClient{config: config, Broker: brokerString, Topic: topicString, ClientID: clientIdString}, nil
+		},
+	})
+}
+
+func (mc *MQTTClient) Id() string {
+	return mc.config.Id
+}
+
+func (mc *MQTTClient) Type() string {
+	return mc.config.Type
+}
+
+func (mc *MQTTClient) RegisterRouter(router *Router) {
+	mc.router = router
+}
+
+func (mc *MQTTClient) Run() error {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(mc.Broker)
+	opts.SetClientID(mc.ClientID)
+	opts.SetAutoReconnect(true)
+	opts.SetCleanSession(false)
+
+	opts.OnConnect = func(c mqtt.Client) {
+		token := mc.client.Subscribe(mc.Topic, 1, func(c mqtt.Client, m mqtt.Message) {
+			mc.router.HandleInput(mc.config.Id, m)
+		})
+		token.Wait()
+	}
+
+	mc.client = mqtt.NewClient(opts)
+
+	token := mc.client.Connect()
+
+	token.Wait()
+	err := token.Error()
+	if err != nil {
+		return err
+	}
+
+	<-mc.router.Context.Done()
+	slog.Debug("router context done in module", "id", mc.config.Id)
+	return nil
+}
+
+func (mc *MQTTClient) Output(payload any) error {
+	return fmt.Errorf("net.mqtt.client output is not implemented")
+}
