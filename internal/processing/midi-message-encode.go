076dc7ae5e77807543007ead49c5cc63bcc25b2d
Joel Wetzell <git@jwetzell.com>
2025-11-23 11:19:44 -0600

    add basic MIDI message encode/decode

---
+++ internal/processing/midi-message-encode.go
@@ -0,0 +1,35 @@
+package processing
+
+import (
+	"context"
+	"fmt"
+
+	"gitlab.com/gomidi/midi/v2"
+)
+
+type MIDIMessageEncode struct {
+	config ProcessorConfig
+}
+
+func (mme *MIDIMessageEncode) Process(ctx context.Context, payload any) (any, error) {
+	payloadMessage, ok := payload.(midi.Message)
+
+	if !ok {
+		return nil, fmt.Errorf("mqtt.message.encode processor only accepts an mqtt.Message")
+	}
+
+	return payloadMessage.Bytes(), nil
+}
+
+func (mme *MIDIMessageEncode) Type() string {
+	return mme.config.Type
+}
+
+func init() {
+	RegisterProcessor(ProcessorRegistration{
+		Type: "midi.message.encode",
+		New: func(config ProcessorConfig) (Processor, error) {
+			return &MIDIMessageEncode{config: config}, nil
+		},
+	})
+}
