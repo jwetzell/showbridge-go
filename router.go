eb0b4686378770941bcbd87c10c9944026eeda41
Joel Wetzell <git@jwetzell.com>
2025-12-01 18:26:13 -0600

    clean out logging

--- router.go
+++ router.go
@@ -161,7 +161,6 @@ func (r *Router) HandleInput(sourceId string, payload any) []RoutingError {
 func (r *Router) HandleOutput(sourceId string, destinationId string, payload any) error {
 	for _, moduleInstance := range r.ModuleInstances {
 		if moduleInstance.Id() == destinationId {
-			slog.Debug("routing", "source", sourceId, "destination", destinationId, "payload", payload)
 			return moduleInstance.Output(payload)
 		}
 	}
