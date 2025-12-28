0248ca6973ffb509fdfb6456fb65a5f7fb29ad27
Joel Wetzell <git@jwetzell.com>
2025-12-28 16:17:56 -0600

    add processor to encode json bytes

---
+++ internal/processor/json-encode.go
@@ -0,0 +1,38 @@
+package processor
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	"github.com/jwetzell/showbridge-go/internal/config"
+)
+
+type JsonEncode struct {
+	config config.ProcessorConfig
+}
+
+func (je *JsonEncode) Process(ctx context.Context, payload any) (any, error) {
+	var payloadBuffer bytes.Buffer
+
+	err := json.NewEncoder(&payloadBuffer).Encode(payload)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return payloadBuffer.Bytes(), nil
+}
+
+func (je *JsonEncode) Type() string {
+	return je.config.Type
+}
+
+func init() {
+	RegisterProcessor(ProcessorRegistration{
+		Type: "json.encode",
+		New: func(config config.ProcessorConfig) (Processor, error) {
+			return &JsonEncode{config: config}, nil
+		},
+	})
+}
