fa612231fb53ed67f06aa222873509e80c4067c5
Joel Wetzell <git@jwetzell.com>
2026-03-13 21:18:32 -0500

    fix otel import path

--- cmd/showbridge/main.go
+++ cmd/showbridge/main.go
@@ -20,7 +20,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 	"sigs.k8s.io/yaml"
 )
 
