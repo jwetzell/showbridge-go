4559c6efbdb4d0510fbf6ae27830fb166f638750
Joel Wetzell <git@jwetzell.com>
2026-03-18 14:18:07 -0500

    add processors for key value operations

---
+++ internal/processor/kv-set.go
@@ -0,0 +1,90 @@
+package processor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"html/template"
+	"log/slog"
+
+	"github.com/jwetzell/showbridge-go/internal/common"
+	"github.com/jwetzell/showbridge-go/internal/config"
+)
+
+type KVSet struct {
+	config   config.ProcessorConfig
+	ModuleId string
+	Key      string
+	Value    *template.Template
+	logger   *slog.Logger
+}
+
+func (kvs *KVSet) Process(ctx context.Context, wrappedPayload common.WrappedPayload) (common.WrappedPayload, error) {
+	ctxModules := ctx.Value(common.ModulesContextKey)
+	if ctxModules == nil {
+		wrappedPayload.End = true
+		return wrappedPayload, errors.New("kv.set unable to get modules from context")
+	}
+
+	moduleMap, ok := ctxModules.(map[string]common.Module)
+	if !ok {
+		wrappedPayload.End = true
+		return wrappedPayload, errors.New("kv.set modules from context has wrong type")
+	}
+
+	module, ok := moduleMap[kvs.ModuleId]
+	if !ok {
+		wrappedPayload.End = true
+		return wrappedPayload, fmt.Errorf("kv.set unable to find module with id: %s", kvs.ModuleId)
+	}
+
+	kvModule, ok := module.(common.KeyValueModule)
+	if !ok {
+		wrappedPayload.End = true
+		return wrappedPayload, fmt.Errorf("kv.set module with id %s is not a KeyValueModule", kvs.ModuleId)
+	}
+
+	err := kvModule.Set(kvs.Key, wrappedPayload.Payload)
+	if err != nil {
+		wrappedPayload.End = true
+		return wrappedPayload, fmt.Errorf("kv.set error setting key: %w", err)
+	}
+
+	return wrappedPayload, nil
+}
+
+func (kvs *KVSet) Type() string {
+	return kvs.config.Type
+}
+
+func init() {
+	RegisterProcessor(ProcessorRegistration{
+		Type: "kv.set",
+		New: func(config config.ProcessorConfig) (Processor, error) {
+
+			params := config.Params
+
+			moduleIdString, err := params.GetString("module")
+			if err != nil {
+				return nil, fmt.Errorf("kv.set module error: %w", err)
+			}
+
+			keyString, err := params.GetString("key")
+			if err != nil {
+				return nil, fmt.Errorf("kv.set key error: %w", err)
+			}
+
+			valueString, err := params.GetString("value")
+			if err != nil {
+				return nil, fmt.Errorf("kv.set value error: %w", err)
+			}
+			valueTemplate, err := template.New("template").Parse(valueString)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return &KVSet{config: config, ModuleId: moduleIdString, Key: keyString, Value: valueTemplate, logger: slog.Default().With("component", "processor", "type", config.Type)}, nil
+		},
+	})
+}
